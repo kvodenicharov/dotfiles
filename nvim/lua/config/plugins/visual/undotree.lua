2a2345acaee14bc58fd1e969ddd378e83145c4b3
Kiril Vodenicharov <kirilvodenicharov@gmail.com>
2024-12-03 14:51:22 +0200

    Fix for undotree not focusing window on open

--- nvim/lua/config/plugins/visual/undotree.lua
+++ nvim/lua/config/plugins/visual/undotree.lua
@@ -4,7 +4,9 @@ return {
 		vim.o.undofile = true
 		vim.o.undodir = vim.fn.stdpath("data") .. "/undo"
 
-		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undo Tree Toggle" })
+		vim.keymap.set("n", "<leader>u", function()
+			vim.cmd.UndotreeToggle()
+			vim.cmd.UndotreeFocus()
+		end, { desc = "Undo Tree Toggle and Focus" })
 	end,
 }
-

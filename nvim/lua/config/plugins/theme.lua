ee879d14a9501bee457760d8c4b51238a60be402
Kiril Vodenicharov <kirilvodenicharov@gmail.com>
2025-01-29 19:25:59 +0200

    More theming

--- nvim/lua/config/plugins/theme.lua
+++ nvim/lua/config/plugins/theme.lua
@@ -1,15 +1,33 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-		style = "storm",
-		transparent = false,
-		terminal_colors = true,
-	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts)
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = "storm",
+			transparent = false,
+			terminal_colors = true,
+		},
+		config = function(_, opts)
+			require("tokyonight").setup(opts)
 
-		vim.cmd([[colorscheme tokyonight]])
-	end,
+			-- vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			background = {
+				dark = "dragon",
+				light = "lotus",
+			},
+		},
+		config = function(_, opts)
+			require("kanagawa").setup(opts)
+
+			vim.cmd([[colorscheme kanagawa]])
+		end,
+	},
 }

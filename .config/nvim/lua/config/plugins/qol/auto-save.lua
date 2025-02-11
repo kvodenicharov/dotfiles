0544e607495c0ced40f57e6c06be576caf716976
Kiril Vodenicharov <kirilvodenicharov@gmail.com>
2025-02-11 17:18:08 +0200

    nvim: Introduce auto-save.nvim plugin

---
+++ .config/nvim/lua/config/plugins/qol/auto-save.lua
@@ -0,0 +1,12 @@
+return {
+	"Pocco81/auto-save.nvim",
+	opts = {
+		execution_message = {
+			message = function()
+				return "AutoSaved"
+			end,
+			dim = 0.18,
+			cleaning_interval = 1250,
+		},
+	},
+}

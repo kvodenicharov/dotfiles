112f24e390ab59878ac886e50973e126374d7210
Kiril Vodenicharov <kirilvodenicharov@gmail.com>
2025-02-24 09:21:38 +0200

    nvim: Fix diagram.lua complaints about imagick

--- .config/nvim/lua/config/plugins/org/diagram.lua
+++ .config/nvim/lua/config/plugins/org/diagram.lua
@@ -1,23 +1,28 @@
 return {
-	"3rd/diagram.nvim",
-	dependencies = {
+	{
 		"3rd/image.nvim",
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {},
 	},
-	config = function()
-		require("image").setup({})
-
-		require("diagram").setup({
-			integrations = {
-				require("diagram.integrations.markdown"),
-				require("diagram.integrations.neorg"),
-			},
-			renderer_options = {
-				mermaid = {
-					background = "transparent",
-					theme = "dark",
-					scale = 2,
+	{
+		"3rd/diagram.nvim",
+		dependencies = {
+			"3rd/image.nvim",
+		},
+		config = function()
+			require("diagram").setup({
+				integrations = {
+					require("diagram.integrations.markdown"),
+					require("diagram.integrations.neorg"),
 				},
-			},
-		})
-	end,
+				renderer_options = {
+					mermaid = {
+						background = "transparent",
+						theme = "dark",
+						scale = 2,
+					},
+				},
+			})
+		end,
+	},
 }

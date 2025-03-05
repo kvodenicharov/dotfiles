095d9195823e113c6f778381f6469575d8c5393c
Kiril Vodenicharov <kirilvodenicharov@gmail.com>
2025-03-05 09:36:18 +0200

    nvim: Auto expand nvim-tree in norg spaces

--- .config/nvim/lua/config/plugins/qol/tree.lua
+++ .config/nvim/lua/config/plugins/qol/tree.lua
@@ -9,6 +9,20 @@ return {
 		require("nvim-tree").setup({})
 
 		local api = require("nvim-tree.api")
+		vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
+			callback = function(args)
+				-- Only proceed if the buffer has a path and is a .norg file
+				local filepath = vim.fn.expand("%:p")
+				if filepath ~= "" and filepath:match("%.norg$") then
+					vim.api.nvim_del_autocmd(args.id)
+					vim.cmd("noautocmd NvimTreeOpen")
+					vim.schedule(function()
+						api.tree.expand_all()
+						vim.cmd("noautocmd wincmd p")
+					end)
+				end
+			end,
+		})
 		vim.keymap.set("n", "<leader>ft", api.tree.toggle, { desc = "File Tree" })
 	end,
 }

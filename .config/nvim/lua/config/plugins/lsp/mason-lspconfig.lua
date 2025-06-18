593d81df5adabb40f11c9e0cca6971effaeecee8
Kiril Vodenicharov <kirilvodenicharov@gmail.com>
2025-06-18 11:14:20 +0300

    nvim: Fix mason config

--- .config/nvim/lua/config/plugins/lsp/mason-lspconfig.lua
+++ .config/nvim/lua/config/plugins/lsp/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
-	"williamboman/mason-lspconfig.nvim",
+	"mason-org/mason-lspconfig.nvim",
 	dependencies = {
-		"williamboman/mason.nvim",
+		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
 		"hrsh7th/cmp-nvim-lsp",
 	},
@@ -11,39 +11,5 @@ return {
 	config = function(_, opts)
 		local mlc = require("mason-lspconfig")
 		mlc.setup(opts)
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local handlers = {
-			-- The first entry (without a key) will be the default handler
-			-- and will be called for each installed server that doesn't have
-			-- a dedicated handler.
-			function(server_name) -- default handler (optional)
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["lua_ls"] = function()
-				local lspconfig = require("lspconfig")
-				lspconfig.lua_ls.setup({
-					settings = {
-						Lua = {
-							runtime = {
-								version = "LuaJIT",
-							},
-							diagnostics = {
-								globals = {
-									"vim",
-									"require",
-								},
-							},
-							workspace = {
-								--library = vim.api.nvim_get_runtime_file("", true),
-							},
-						},
-					},
-				})
-			end,
-		}
-
-		mlc.setup_handlers(handlers)
 	end,
 }
